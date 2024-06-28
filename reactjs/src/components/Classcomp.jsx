87fae2530575a25a9b164f787d9c677188a5cee5
meet <chudasamameet4152@gmail.com>
2024-06-28 12:17:31 +0530

    CRUD Operation

---
+++ reactjs/src/components/Classcomp.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Classcomp extends Component{
+    render() {
+      return (
+        <div>
+          <>
+            <h1>Lala Don</h1>
+          </>
+        </div>
+      )
+    }
+}
+
+export default Classcomp
---
+++ reactjs/src/components/Funcomp.jsx
@@ -0,0 +1,19 @@
+// function Funcomp (){
+//     return(
+//         <div>
+//             <h1>Chudasama</h1>
+//         </div>
+//     )
+// }
+
+// export default Funcomp  
+
+const Funcomp = () =>{
+    return (
+      <>
+        <h1>Gujju Don</h1>
+      </>
+    );
+}
+
+export default Funcomp
